8e1e342dfbf2887da8747cd490b56a50843b4389
njansh <nadsonjhonyobmep@gmail.com>
2025-12-17 20:23:38 -0300

    feat: modelo Livro com estado inicial e validações

---
+++ Sistema-de-Biblioteca/src/com/br/nadson/sistema_de_biblioteca/enums/EstadoLivro.java
@@ -0,0 +1,5 @@
+package com.br.nadson.sistema_de_biblioteca.enums;
+
+public enum EstadoLivro {
+    DISPONIVEL,EMPRESTADO;
+}
---
+++ Sistema-de-Biblioteca/src/com/br/nadson/sistema_de_biblioteca/modelo/Livro.java
@@ -0,0 +1,36 @@
+package com.br.nadson.sistema_de_biblioteca.modelo;
+
+import com.br.nadson.sistema_de_biblioteca.enums.EstadoLivro;
+
+public class Livro {
+    private EstadoLivro estadoLivro;
+    private String titulo;
+    private String autor;
+    private int isbn;
+
+    public Livro(String titulo, String autor, int isbn) {
+        if(titulo == null || titulo.isBlank()) {
+            throw new IllegalArgumentException("Titulo inválido");
+
+        }
+        if(autor == null || autor.isBlank()) {
+            throw new IllegalArgumentException("Autor inválido");
+
+        }
+        if(isbn <= 0) {
+            throw new IllegalArgumentException("ISBN inválido");
+
+        }
+
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.estadoLivro = EstadoLivro.DISPONIVEL;
+    }
+
+    public EstadoLivro getEstadoLivro() {
+        return estadoLivro;
+    }
+
+
+}
