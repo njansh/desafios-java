f086823092ff2a44581e4bd427e9bb36e34f919c
njansh <nadsonjhonyobmep@gmail.com>
2025-12-18 17:47:39 -0300

    mudando para o outro notbook

--- Sistema-de-Biblioteca/src/com/br/nadson/sistema_de_biblioteca/modelo/Usuario.java
+++ Sistema-de-Biblioteca/src/com/br/nadson/sistema_de_biblioteca/modelo/Usuario.java
@@ -1,20 +1,45 @@
 package com.br.nadson.sistema_de_biblioteca.modelo;
 
+import com.br.nadson.sistema_de_biblioteca.enums.EstadoLivro;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario extends Pessoa{
     private static int GERADOR_MATRICULA = 1;
     private final int matricula;
+    private List<Livro> livros;
 
 
     public Usuario(String nome, int idade) {
         super(nome, idade);
         this.matricula = GERADOR_MATRICULA++;
+        livros=new ArrayList<>();
 
     }
 
     public int getMatricula() {
         return matricula;
-;
+
     }
+public void emprestarLivro(Livro livro){
+        if (livro==null){
+            throw new IllegalArgumentException("Livro invalido");
+        }
+        if(livros.contains(livro){
+    }
+        livro.emprestar();
+        livros.add(livro);
+}
+public void devolverLivro(Livro livro){
+        if(livro==null){
+            throw new IllegalArgumentException("Livro invalido");
+
+        }
+
+        livro.devolver();
+        livros.remove(livro);
+}
 
     @Override
     public String apresentar(){
