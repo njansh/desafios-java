b47d9c0108b8e8e30dfe0eec1bb2c8de44fb601f
Njansh <nadsonjhonyobmep@gmail.com>
2025-12-19 19:27:18 -0300

    feat: adiciona regras de negócio ao Pedido

--- sitema-de-pedidos/src/br/com/nadson/sistema_de_pedidos/modelo/Pedido.java
+++ sitema-de-pedidos/src/br/com/nadson/sistema_de_pedidos/modelo/Pedido.java
@@ -20,13 +20,58 @@ public class Pedido {
         if (item==null){
             throw new IllegalStateException("Item não pode ser nulo");
         }
+        if (status!=StatusPedido.CRIADO){
+            throw new IllegalStateException("Pedido não pode ser modificado depois de criado");
+        }if(itens.contains(item)){
+            throw new IllegalStateException("Item já está no pedido");
+        }
         this.itens.add(item);
 
     }
+    public void removerItem(ItemPedido item){
+        if (item==null){
+            throw new IllegalStateException("Item não pode ser nulo");
+        }
+        if(status!=StatusPedido.CRIADO){
+            throw new IllegalStateException("Pedido não pode ser modificado depois de criado");
+        }
 
+        if (itens.contains(item)){
+            this.itens.remove(item);
+        }else{
+            throw new IllegalStateException("Item não está no pedido");
+        }
+    }
 
+    public double getValorTotal(){
+        return itens.stream().mapToDouble(ItemPedido::getValorTotal).sum();
+    }
 
     public Cliente getCliente() {
         return cliente;
+    }
+
+    public List<ItemPedido> getItens() {
+    return new ArrayList<>(itens);
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPedido status) {
+        if (status == null) {
+            throw new IllegalArgumentException("Status não pode ser nulo");
+        }
+        if (this.status == StatusPedido.ENTREGUE || this.status == StatusPedido.CANCELADO) {
+            throw new IllegalStateException("Não é possível alterar o status de um pedido entregue ou cancelado.");
+        }
+        if (this.status == StatusPedido.CRIADO && status == StatusPedido.ENTREGUE) {
+            throw new IllegalStateException("Não é possível ir de CRIADO para ENTREGUE diretamente.");
+        }
+        if (this.status == StatusPedido.PAGO && status == StatusPedido.CRIADO) {
+            throw new IllegalStateException("Não é possível reverter um pedido pago para CRIADO.");
+        }
+        this.status = status;
     }
 }
